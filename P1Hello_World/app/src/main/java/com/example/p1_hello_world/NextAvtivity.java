32cb573b37e59b3464502d096ea4a4f0ae2e16b4
iqbalwaldan <iqbalwaldan0510@gmail.com>
2021-08-04 10:36:54 +0700

    first commit

---
+++ P1Hello_World/app/src/main/java/com/example/p1_hello_world/NextAvtivity.java
@@ -0,0 +1,13 @@
+package com.example.p1_hello_world;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NextAvtivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next_avtivity);
+    }
+}
---
+++ P2Starter/dtschapter02-starter-master/app/src/main/java/polinema/ac/id/chapter02starter/LinierLayout.java
@@ -0,0 +1,14 @@
+package polinema.ac.id.chapter02starter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LinierLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linier_layout);
+    }
+}
