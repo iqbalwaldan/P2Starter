d33aee417a6f30d8f3a1645c532f7ca3874ab64d
iqbalwaldan <iqbalwaldan0510@gmail.com>
2021-08-15 23:55:57 +0700

    menambahkan menu register MyFilm

--- P7Tugas/MyFilm/app/src/main/java/com/example/myfilm/Register.java
+++ P7Tugas/MyFilm/app/src/main/java/com/example/myfilm/Register.java
@@ -10,10 +10,11 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+public class Register extends AppCompatActivity {
 
     EditText editTextEmail;
     EditText editTextPassword;
+    EditText editTextConfirmPassword;
 
     public static boolean isValidEmail(CharSequence email) {
         return (Patterns.EMAIL_ADDRESS.matcher(email).matches());
@@ -23,21 +24,13 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
         editTextEmail = findViewById(R.id.inp_txt_email);
         editTextPassword = findViewById(R.id.inp_txt_password);
+        editTextConfirmPassword = findViewById(R.id.edt_confirm_password);
     }
 
-    public void clickForgot(View view) {
-        Intent i = new Intent(Login.this, ForgotPassword.class);
-        startActivity(i);
-    }
-    public void clickRegister(View view) {
-        Intent i = new Intent(Login.this, Register.class);
-        startActivity(i);
-    }
-
-    public void postLogin(View view) {
+    public void postRegister(View view) {
         if(TextUtils.isEmpty(editTextEmail.getText().toString().trim()) &&
                 TextUtils.isEmpty(editTextPassword.getText().toString().trim())) {
             Toast.makeText(view.getContext(), "Email dan Password tidak boleh kosong!",
@@ -48,8 +41,12 @@ public class Login extends AppCompatActivity {
             Toast.makeText(view.getContext(), "Email tidak valid!", Toast.LENGTH_LONG).show();
         } else if(TextUtils.isEmpty(editTextPassword.getText().toString())) {
             Toast.makeText(view.getContext(), "Password tidak boleh kosong!", Toast.LENGTH_LONG).show();
-        } else {
-            Intent i = new Intent(Login.this, MainActivity.class);
+        } else if (TextUtils.getTrimmedLength(editTextPassword.getText().toString()) > 15){
+            Toast.makeText(view.getContext(), "Password melebihi batas!", Toast.LENGTH_LONG).show();
+        } else if (!TextUtils.equals(editTextPassword.getText().toString(),editTextConfirmPassword.getText().toString())){
+            Toast.makeText(view.getContext(), "Password tidak sama!", Toast.LENGTH_LONG).show();
+        }else {
+            Intent i = new Intent(Register.this, SuccesRegister.class);
             startActivity(i);
         }
     }
